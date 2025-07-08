b654832e9e8d7b89d3d4d61cef01a116a77e3f60
dozie-oc <111375002+dozie-oc@users.noreply.github.com>
2025-07-08 13:13:50 +0100

    Job filtering and grammar correction

--- project.sql/in_demand_skills.sql
+++ project.sql/in_demand_skills.sql
@@ -6,6 +6,7 @@ WITH top_skills AS (
     FROM skills_dim s
     INNER JOIN skills_job_dim sj ON s.skill_id = sj.skill_id
     INNER JOIN job_postings_fact j ON sj.job_id = j.job_id
+    WHERE job_title_short = 'Data Scientist'
     GROUP BY 
         s.skill_id,
         skills
--- project.sql/top_paying_skills.sql
+++ project.sql/top_paying_skills.sql
@@ -1,6 +1,6 @@
 /*Now this query is meant to show the skills reqired for the 
 previously queried highest paying data scientist jobs, allowing
-one see what skills are needed for the highes paying remote jobs
+one see what skills are needed for the highest paying remote jobs
 available.*/
 
 WITH top_paying_jobs AS (
@@ -20,7 +20,8 @@ WITH top_paying_jobs AS (
 
 SELECT
     tpj.*,
-    STRING_AGG(skills,',' ORDER BY skills) AS required_skills
+    STRING_AGG(skills,',' ORDER BY skills) AS required_skills,
+    COUNT(*)
 FROM top_paying_jobs tpj
 INNER JOIN skills_job_dim sj ON sj.job_id = tpj.job_id
 INNER JOIN skills_dim s ON s.skill_id = sj.skill_id
